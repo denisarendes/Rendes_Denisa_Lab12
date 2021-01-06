99d7ee458a9568203a19d01822288e48fec4455a
DENISA RENDEŞ <denisa.rendes@stud.ubbcluj.ro>
2021-01-06 14:14:31 +0200

    Add project files.

---
+++ Rendes_Denisa_Lab12/Rendes_Denisa_Lab12/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rendes_Denisa_Lab12.Data;
+
+namespace Rendes_Denisa_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+            //InitializeComponent();
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
